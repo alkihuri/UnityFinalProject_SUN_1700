9d8d5786b2a8847d15c724f9549ef85e41402661
ieeooo <95179246+ieeooo@users.noreply.github.com>
2022-01-30 18:25:35 +0300

    animation files

---
+++ FinalProject.Untiy/Assets/Inputs/LeonidLaptev/scripts/AnimatorController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    public Animator characterAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            characterAnim.SetBool("iswalk",true);
+        }
+        else
+        {
+            characterAnim.SetBool("iswalk",false);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            characterAnim.SetBool("iswalkback",true);
+        }
+        else
+        {
+            characterAnim.SetBool("iswalkback",false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            characterAnim.SetBool("iswalkleft",true);
+        }
+        else
+        {
+            characterAnim.SetBool("iswalkleft",false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            characterAnim.SetBool("iswalkright",true);
+        }
+        else
+        {
+            characterAnim.SetBool("iswalkright",false);
+        }
+
+
+
+
+    }
+}
