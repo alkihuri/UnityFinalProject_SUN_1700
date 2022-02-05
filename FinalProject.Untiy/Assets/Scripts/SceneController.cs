24d92c1003c3c79f9c4561470f069444a8290f6d
ieeooo <95179246+ieeooo@users.noreply.github.com>
2022-02-05 19:12:15 +0300

    fps camera and starting scene

---
+++ FinalProject.Untiy/Assets/Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void mainScreen()
+    {
+        SceneManager.LoadScene("GameDemo");
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
