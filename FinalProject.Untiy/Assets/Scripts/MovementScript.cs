6505cd9d298b137ee0fb79b04bc05d4e99d1176f
sSheral <95179382+sSheral@users.noreply.github.com>
2022-02-20 17:13:50 +0200

    Shift + esc button

--- FinalProject.Untiy/Assets/Scripts/MovementScript.cs
+++ FinalProject.Untiy/Assets/Scripts/MovementScript.cs
@@ -16,10 +16,12 @@ public class MovementScript : MonoBehaviour
     private float yCameraAngle;
     private float xCameraAngle;
     private float _yAngleRestriction = 45;
-     
+    private float speedBoost = 2;
+    public GameObject showEscapePanel;
 
-    // Update is called once per frame
-    void FixedUpdate()
+                
+// Update is called once per frame
+void FixedUpdate()
     {
         // Check if player is mine
         if (!GetComponent<PhotonView>().IsMine)
@@ -42,7 +44,12 @@ public class MovementScript : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             //Multiply it by speed.
-            moveDirection *= speed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                moveDirection *= speed * speedBoost;
+            }
+            else
+                moveDirection *= speed;
             //Jumping
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
@@ -50,6 +57,15 @@ public class MovementScript : MonoBehaviour
         }
         turner = Input.GetAxis("Mouse X") * sensitivity;
         looker = -Input.GetAxis("Mouse Y") * sensitivity;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            showEscapePanel.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            showEscapePanel.SetActive(false);
+        }
 
         if (turner != 0)
         {
