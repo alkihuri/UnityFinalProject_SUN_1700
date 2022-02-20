82e6fab55dc589ed713b58a63a6345314efaac31
alkihuri <alkihuri@gmail.com>
2022-02-20 17:49:24 +0200

    Gun Logic script  template

--- FinalProject.Untiy/Assets/Scripts/GameCore/Mechanics/Shooting/GunLogic.cs
+++ FinalProject.Untiy/Assets/Scripts/GameCore/Mechanics/Shooting/GunLogic.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GunLogic : MonoBehaviour
 {
+    [SerializeField] GameObject _camera;
+    Vector3 _shootDirection;
+    Vector3 _originPoint;
+    Vector3 _attentionPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /// camera innit
+    }
+
+    private void GetRaycastInputVectors()
+    {
+        _shootDirection = _camera.transform.forward;
+        _originPoint = _camera.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetRaycastInputVectors();
+        RaycastHit objectOnHitLine; 
+        if(Physics.Raycast(_originPoint,_shootDirection,out objectOnHitLine))
+        {
+            _attentionPoint = objectOnHitLine.point;
+        }
         
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_attentionPoint, 5);
     }
 }
